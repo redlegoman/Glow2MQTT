4a3b04621a94942e6ee4cf255ec506e49fad82f8
Andy McDade <andy@heating>
2021-09-10 12:42:36 +0100

    added gas, so also split the mqtt topics

--- consumption.sh
+++ consumption.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-glowuser="<CHANGEME>"
-glowpass="<CHANGEME>"
-mqttip="<CHANGEME>"
-mqttuser="<CHANGEME>"
-mqttpass="<CHANGEME>"
-consumption=`glowmarkt-today -u $glowuser -p $glowpass -c electricity.consumption`
-cost=`glowmarkt-today -u $glowuser -p $glowpass -c electricity.consumption.cost`
-mosquitto_pub -h $mqttip -m $consumption -t glowmarkt/consumptiontoday -u $mqttuser -P $mqttpass -r -d
-mosquitto_pub -h $mqttip -m $cost -t glowmarkt/costtoday -u $mqttuser -P $mqttpass -r -d
+glowuser=""
+glowpass=""
+mqttip=""
+mqttuser=""
+mqttpass=""
+e_consumption=`glowmarkt-today -u $glowuser -p $glowpass -c electricity.consumption`
+e_cost=`glowmarkt-today -u $glowuser -p $glowpass -c electricity.consumption.cost`
+mosquitto_pub -h $mqttip -m $e_consumption -t glowmarkt/electric_consumptiontoday -u $mqttuser -P $mqttpass -r -d
+mosquitto_pub -h $mqttip -m $e_cost -t glowmarkt/electric_costtoday -u $mqttuser -P $mqttpass -r -d
+g_consumption=`glowmarkt-today -u $glowuser -p $glowpass -c gas.consumption`
+g_cost=`glowmarkt-today -u $glowuser -p $glowpass -c gas.consumption.cost`
+mosquitto_pub -h $mqttip -m $g_consumption -t glowmarkt/gas_consumptiontoday -u $mqttuser -P $mqttpass -r -d
+mosquitto_pub -h $mqttip -m $e_cost -t glowmarkt/gas_costtoday -u $mqttuser -P $mqttpass -r -d
